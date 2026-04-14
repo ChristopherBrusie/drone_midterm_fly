488a6894a7a99b2b292a038945b241471ed531ae
Chris Brusie <143448429+ChristopherBrusie@users.noreply.github.com>
2026-04-13 19:51:18 -0500

    mm

--- matlab/kalman_filter.m
+++ matlab/kalman_filter.m
@@ -2,6 +2,9 @@
 % 1. assuming constant velocity, simple integrator dynamics. 
 % states 3, 4 are constant.
 
+clear all
+close all
+
 sigma = 0.5;
 
 R = eye(2).*(sigma^2);
@@ -27,7 +30,7 @@ B_d = sys_disc.B;
 
 
 % 4 initial state and covariance
-x0 = [0;0;0;0];
+x0 = [0;0;1;1];
 P0 = Q;
 % justification: i don't know
 
@@ -40,7 +43,18 @@ y_trial_2 = [3.7683 4.7671 6.1210 6.0434 7.1375;
             2.7189 4.4936 7.1571 8.5460 10.2938];
 
 
-G = [0 0;
-     0 0;
-     Ts 0;
-     0 Ts];
+
+% propogate discrete synamics (exponent)
+state_pred = [];
+for i = 1:5
+    state_pred(:, i) = A_d * x0;
+    x0 = state_pred(:, i); % Update state for next iteration
+end
+
+
+% plotting x position for trial 1
+tspace = 1:Ts:5;
+plot(tspace, y_trial_2(1,:), 'o', 'LineWidth', 2, 'MarkerSize', 12)
+hold on
+plot(state_pred(1,:), tspace, 'LineWidth', 2)
+
